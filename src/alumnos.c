7bf350e99e052f0ce66acfe050dbfef610a7d786
Ivan <ivanleonelszk@hotmail.com>
2020-06-30 20:34:25 -0300

    Se agregan los datos del alumno SZKRABKO, Ivan

--- src/alumnos.c
+++ src/alumnos.c
@@ -46,4 +46,14 @@ bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno) {
     return (resultado >= 0);
 }
 
+bool IvanSzkrabko(char * cadena, size_t espacio) {
+    const struct alumno_s alumno = {
+        .apellidos = "SZKRABKO",
+        .nombres = "Ivan",
+        .documento = "77.777.77",
+    };
+
+    return SerializarAlumno(cadena, espacio, &alumno);
+}
+
 /*=====[Implementations of private functions]================================*/
