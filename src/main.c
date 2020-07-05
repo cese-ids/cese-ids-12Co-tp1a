f1c3448371d44d968f4c4cdf2a4920c2bec7b946
Pablo Daniel Folino <pfolino@gmail.com>
2020-07-05 17:47:13 -0300

    Se agregan los datos del alumno FOLINO, Pablo Daniel

--- src/main.c
+++ src/main.c
@@ -35,5 +35,13 @@
 /*=====[Implementations of private functions]================================*/
 
 int main(int argc, char *argv[]) {
+    char cadena[30];
+    
+    if (PabloFolino(cadena, sizeof(cadena))) {
+        printf("Alumno: %s\r\n", cadena);
+    } else {
+        printf("No se pudo serializar el alumno\r\n");
+    }
+
     return 0;
 }
