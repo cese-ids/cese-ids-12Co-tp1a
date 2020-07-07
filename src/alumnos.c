bff4cf9fa5de001744efa1f087e589f2e6f1fc31
Fernando Daniele <fernandodaniele1993@gmail.com>
2020-07-06 22:34:18 -0300

    Se agregan los datos del alumno DANIELE, Fernando

--- src/alumnos.c
+++ src/alumnos.c
@@ -46,4 +46,13 @@ bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno) {
     return (resultado >= 0);
 }
 
+bool FernandoDaniele(char * cadena, size_t espacio) {
+    const struct alumno_s alumno = {
+        .apellidos = "DANIELE",
+        .nombres = "Fernando Ezequiel",
+        .documento = "37127229",
+    };
+
+    return SerializarAlumno(cadena, espacio, &alumno);
+}
 /*=====[Implementations of private functions]================================*/
